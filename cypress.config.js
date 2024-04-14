f9e3afc87fb4afe2bf020378ac5276fc42c27d07
camilapessoa <camilabentespessoa@gmail.com>
2024-04-14 13:16:16 -0300

    configuracoes

---
+++ cypress.config.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:3000/',
+    video: true,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: true,
+      json: false,
+      timestamp: "mmddyyyy_HHMMss"
+    },
+
+  },
+
+  env:
+  {
+    "email": "clinica@gmail.com",
+    "senha": "4321",
+    "api_login": "http://localhost:8080/auth/login",
+    "api_clinica": "http://localhost:8080/clinica",
+    "api_especialista": "http://localhost:8080/especialista",
+    "requestMode": true
+  }
+
+});
