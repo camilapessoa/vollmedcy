f7b165f8d5a51fef8f7a49ce09fdad6ef9b90575
camilapessoa <camilabentespessoa@gmail.com>
2024-04-16 13:15:29 -0300

    github actions ci cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -16,7 +16,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    // projectId: 'x3bmp5',
+    projectId: "k2iz8s",
     baseUrl: 'http://localhost:3000/',
     video: false,
     // video: true,
